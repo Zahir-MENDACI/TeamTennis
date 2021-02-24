7bf7a27dae310bad3439f3207c6a76dd22f6ac81
Zahir <menzahir@gmail.com>
2021-02-24 19:41:18 +0100

    Navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // https://reactrouter.com/web/example/basic
 import './Navbar.scss';
+import { ReactComponent as CloseMenu } from "./x.svg";
+import { ReactComponent as MenuIcon } from "./menu.svg";
 import {NavLink} from "react-router-dom"
+
+
 
 const Navbar = () => {
     function toggleMobileMenu (){
         document.querySelector('#menu').classList.toggle('active')
         document.querySelector('.mobile-bar').classList.toggle('active')
     }
-    return (
-            <div class="Navbar">
-                <input type="checkbox" id="nav-check"/>
 
-                <div class="nav-btn">
-                    <label for="nav-check">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    </label>
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+
+
+    return (
+
+        
+        
+            <div class="Navbar">
+
+
+
+                <div className="mobile-menu" onClick={handleClick}>
+                    {click ? (
+                    <CloseMenu className="menu-icon" />
+                    ) : (
+                    <MenuIcon className="menu-icon" />
+                    )}
                 </div>
-                
-                <div class="nav-links">
-                    <NavLink exact to = "/">Accueil</NavLink> 
-                    <NavLink exact to = "Coach">Club</NavLink>
-                    <NavLink exact to = "/">Planning</NavLink> 
-                    <NavLink exact to = "/">S'inscrire</NavLink> 
-                    <NavLink exact to = "Abonnement">Abonnements</NavLink>
-                    <NavLink exact to = "/">Contact</NavLink> 
-                    <NavLink exact to = "/">Covid-19</NavLink> 
-                </div>
+                <nav>
+                    <ul className={click ? "nav-options active" : "nav-options"}>
+                        <li>
+                            <NavLink exact to = "/" onClick={closeMobileMenu}>Accueil</NavLink> 
+                        </li>
+                        <li>
+                            <NavLink exact to = "Coach" onClick={closeMobileMenu}>Club</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to = "/Planning" onClick={closeMobileMenu}>Planning</NavLink> 
+                        </li>
+                        <li>
+                            <NavLink exact to = "/Inscription" onClick={closeMobileMenu}>S'inscrire</NavLink> 
+                        </li>
+                        <li>
+                            <NavLink exact to = "Abonnement" onClick={closeMobileMenu}>Abonnements</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to = "/Contact" onClick={closeMobileMenu}>Contact</NavLink> 
+                        </li>
+                        <li>
+                            <NavLink exact to = "/Covid19" onClick={closeMobileMenu}>Covid-19</NavLink> 
+                        </li>
+                    </ul>
+                </nav>
             </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+//import Footer from '../components/Footer';
+import Footer from '../Acceuil/Footer';
+import Banner from '../Acceuil/Banner';
+import Info from '../Acceuil/Info';
+import Coach from '../Acceuil/Coach';
+
 
 const Home = () => {
     return (
         <>
             <Navbar/>
-            <div className="Home">
+            {/* <div className="Home">
                 <div class="btndiv">
                     <button class="button">S'inscrire à un cours</button>
                     <button class="button">Prendre un abonnement</button>
                 </div>
             </div>
-            <Footer />
+            <Footer /> */}
+                <Banner />
+                <Info />
+                <Coach />
+                <Footer />
         </>
     );
 };
